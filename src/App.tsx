f7c0d3fe2512db5d8e9c99c6281574a166978746
Modest Team <elbotir2008@gmail.com>
2024-02-10 21:48:36 +0500

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [result, setResult] = useState({
+    numbers: "0",
+    deeds: "",
+  });
+
+  const setResultNumber = (number: any) => {
+    if (result.deeds) {
+      setResult({
+        numbers: number,
+        deeds: result.deeds,
+      });
+    } else {
+      setResult((prevResult) => ({
+        ...prevResult,
+        numbers:
+          prevResult.numbers === "0" ? number : prevResult.numbers + number,
+      }));
+    }
+  };
+
+  const performOperation = (operation: any) => {
+    setResult((prevResult) => ({
+      ...prevResult,
+      deeds: operation,
+    }));
+  };
+
+  const clearResult = () => {
+    setResult({
+      numbers: "0",
+      deeds: "",
+    });
+  };
+
+  const calculateResult = () => {
+    switch (result.deeds) {
+      case "+":
+        setResult((prevResult) => ({
+          numbers: `${
+            parseFloat(prevResult.numbers) + parseFloat(result.numbers)
+          }`,
+          deeds: "",
+        }));
+        break;
+      case "-":
+        setResult((prevResult) => ({
+          numbers: `${
+            parseFloat(prevResult.numbers) - parseFloat(result.numbers)
+          }`,
+          deeds: "",
+        }));
+        break;
+      case "*":
+        setResult((prevResult) => ({
+          numbers: `${
+            parseFloat(prevResult.numbers) * parseFloat(result.numbers)
+          }`,
+          deeds: "",
+        }));
+        break;
+      case "/":
+        setResult((prevResult) => ({
+          numbers: `${
+            parseFloat(prevResult.numbers) / parseFloat(result.numbers)
+          }`,
+          deeds: "",
+        }));
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="calculator">
+        <div className="output">
+          <span className="result">{result.numbers}</span>
+        </div>
+        <div className="buttons">
+          <button onClick={() => setResultNumber("1")}>1</button>
+          <button onClick={() => setResultNumber("2")}>2</button>
+          <button onClick={() => setResultNumber("3")}>3</button>
+          <button onClick={() => performOperation("+")}>+</button>
+          <button onClick={() => setResultNumber("4")}>4</button>
+          <button onClick={() => setResultNumber("5")}>5</button>
+          <button onClick={() => setResultNumber("6")}>6</button>
+          <button onClick={() => performOperation("-")}>-</button>
+          <button onClick={() => setResultNumber("7")}>7</button>
+          <button onClick={() => setResultNumber("8")}>8</button>
+          <button onClick={() => setResultNumber("9")}>9</button>
+          <button onClick={() => performOperation("*")}>*</button>
+          <button className="bg-red" onClick={clearResult}>
+            C
+          </button>
+          <button onClick={() => setResultNumber("0")}>0</button>
+          <button className="bg-green" onClick={calculateResult}>
+            =
+          </button>
+          <button onClick={() => performOperation("/")}>/</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
